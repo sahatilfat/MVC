19db3e987db62bf087c7a724b66d80c726699a92
sahatilfat <sahatilfat@gmail.com>
2021-11-08 02:14:11 +0700

    controller

---
+++ app/controllers/About.php
@@ -0,0 +1,14 @@
+<?php
+
+class About
+{
+
+    public function index($nama = 'tafli', $profesi = 'programmer')
+    {
+        echo "halo nama saya $nama dan saya seorang $profesi";
+    }
+    public function page()
+    {
+        echo 'About/page';
+    }
+}
