6e4170e5642239d3ffe7edb11c2c9da97c718308
sahatilfat <sahatilfat@gmail.com>
2021-11-09 10:59:08 +0700

    Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,8 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama'] ?> dan saya berumur <?= $data['umur'] ?> dan saya sebagai <?= $data['pekerjaan'] ?></p>
+<div class="container">
+
+    <h1 class='mt-4'>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/gambar.png" alt="M Tafli Tahas" width="200vw" class='rounded-circle shadow'>
+    <p>Halo, nama saya <?= $data['nama'] ?> dan saya berumur <?= $data['umur'] ?> dan saya sebagai <?= $data['pekerjaan'] ?></p>
+
+
+</div>
